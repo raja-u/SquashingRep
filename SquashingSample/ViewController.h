b03a7240f3b6f7dfc961a06dfdfb4a3967e9491b
rajasekhar.uppari@netzen-business-solutions.com <raja-u>
2016-02-01 19:35:46 +0530

    Initial Commit

---
+++ SquashingSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SquashingSample
+//
+//  Created by Raja Sekhar on 01/02/16.
+//  Copyright © 2016 Mobi Esprits Software Pvt. Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SquashingSample/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SquashingSample
+//
+//  Created by Raja Sekhar on 01/02/16.
+//  Copyright © 2016 Mobi Esprits Software Pvt. Ltd. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
